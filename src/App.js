6491683ebce147761026d49264090b63b6c82a94
ayush raj <ayushrajkushwaha@gmail.com>
2019-01-08 16:02:16 +0530

    Third Commit

--- src/App.js
+++ src/App.js
@@ -1,31 +1,16 @@
-import React from 'react'
+import React, {Component} from 'react';
+import './App.css';
 
-class App extends React.Component {
-    constructor() {
-        super()
-        this.state = {
-            count: 0
-        }
-        this.addHandle = this.addHandle.bind(this)
-    }
-
-    addHandle(id) {
-        this.setState(prev => {
-                return {
-                    count: id ? prev.count + 1 : prev.count - 1
-                }
-            }
-        )
-    }
-
+class App extends Component {
     render() {
         return (
-            <div>
-                <h1>Hi, You Count is {this.state.count}</h1>
-                <button onClick={() => this.addHandle(true)}> Add</button>
-                <button onClick={() => this.addHandle(false)}> Subtract</button>
+            <div className="App">
+                    <p>
+                       APP AYUSH
+                    </p>
+
             </div>
-        )
+        );
     }
 }
 
